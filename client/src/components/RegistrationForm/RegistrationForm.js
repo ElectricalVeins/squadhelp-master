6efea9dc418bdcea0abdc3d57c576cf37b42de3c
eveins <maxmost512@gmail.com>
2020-04-03 15:32:34 +0300

    fix: registration page and form

--- client/src/components/RegistrationForm/RegistrationForm.js
+++ client/src/components/RegistrationForm/RegistrationForm.js
@@ -1,151 +1,111 @@
-import React                             from 'react';
-import Error                             from '../Error/Error';
-import { connect }                       from 'react-redux';
-import { authActionRegister, clearAuth } from '../../actions/actionCreator';
-import styles                            from './RegistrationForm.module.sass';
-import { Field, reduxForm }              from 'redux-form';
-import FormInput                         from '../FormInput/FormInput';
-import RoleInput                         from '../RoleInput/RoleInput';
+import React                from 'react';
+import styles               from './RegistrationForm.module.sass';
+import { Field, reduxForm } from 'redux-form';
+import FormInput            from '../FormInput/FormInput';
+import RoleInput            from '../RoleInput/RoleInput';
 import AgreeTermOfServiceInput
-                                         from '../AgreeTermOfServiceInput/AgreeTermOfServiceInput';
-import CONSTANTS                         from '../../constants';
-import customValidator                   from '../../validators/validator';
-import Schemes                           from '../../validators/validationSchems';
+                            from '../AgreeTermOfServiceInput/AgreeTermOfServiceInput';
+import CONSTANTS            from '../../constants';
+import customValidator      from '../../validators/validator';
+import Schemes              from '../../validators/validationSchems';
 
-class RegistrationForm extends React.Component {
+const RegistrationForm = props => {
 
-  componentWillUnmount() {
-    this.props.authClear();
-  }
+  const { handleSubmit, submitting } = props;
 
-  handleSubmit = ( values ) => {
-    this.props.signUp( {
-      firstName: values.firstName,
-      lastName: values.lastName,
-      displayName: values.displayName,
-      email: values.email,
-      password: values.password,
-      role: values.role,
-    } );
+  const formInputClasses = {
+    containerStyles: styles.inputContainer,
+    inputStyles: styles.input,
+    warningStyles: styles.fieldWarning,
+    invalidStyles: styles.notValid,
+    validStyles: styles.valid,
   };
 
-  render() {
-    const { handleSubmit, submitting, authClear,auth:{error} } = this.props;
+  return (
+    <form className={styles.signUpFormContainer}
+          onSubmit={handleSubmit}>
+      <div className={styles.row}>
+        <Field
+          name='firstName'
+          {...formInputClasses}
+          component={FormInput}
+          type='text'
+          label='First name'
+        />
+        <Field
+          name='lastName'
+          {...formInputClasses}
+          component={FormInput}
+          type='text'
+          label='Last name'
+        />
+      </div>
+      <div className={styles.row}>
+        <Field
+          name='displayName'
+          {...formInputClasses}
+          component={FormInput}
+          type='text'
+          label='Display Name'
+        />
+        <Field
+          name='email'
+          {...formInputClasses}
+          component={FormInput}
+          type='text'
+          label='Email Address'
+        />
+      </div>
+      <div className={styles.row}>
+        <Field
+          name='password'
+          {...formInputClasses}
+          component={FormInput}
+          type='password'
+          label='Password'
+        />
+        <Field
+          name='confirmPassword'
+          {...formInputClasses}
+          component={FormInput}
+          type='password'
+          label='Password confirmation'
+        />
+      </div>
+      <div className={styles.choseRoleContainer}>
+        <Field name='role' type='radio' value={CONSTANTS.CUSTOMER}
+               strRole='Join As a Buyer'
+               infoRole='I am looking for a Name, Logo or Tagline for my business, brand or product.'
+               component={RoleInput} id={CONSTANTS.CUSTOMER}/>
+        <Field name='role' type='radio' value={CONSTANTS.CREATOR}
+               strRole='Join As a Creative'
+               infoRole='I plan to submit name ideas, Logo designs or sell names in Domain Marketplace.'
+               component={RoleInput} id={CONSTANTS.CREATOR}/>
+      </div>
+      <div className={styles.termsOfService}>
+        <Field
+          name='agreeOfTerms'
+          classes={{
+            container: styles.termsOfService,
+            warning: styles.fieldWarning,
+          }}
+          id='termsOfService'
+          component={AgreeTermOfServiceInput}
+          type='checkbox'
+        />
 
-    const formInputClasses = {
-      containerStyles: styles.inputContainer,
-      inputStyles: styles.input,
-      warningStyles: styles.fieldWarning,
-      invalidStyles: styles.notValid,
-      validStyles: styles.valid,
-    };
-
-    return (
-      <>
-        {error && <Error data={error.data}
-                         status={error.status}
-                         clearError={authClear}/>}
-
-        <form className={styles.signUpFormContainer}
-              onSubmit={handleSubmit( this.handleSubmit )}>
-          <div className={styles.row}>
-            <Field
-              name='firstName'
-              {...formInputClasses}
-              component={FormInput}
-              type='text'
-              label='First name'
-            />
-            <Field
-              name='lastName'
-              {...formInputClasses}
-              component={FormInput}
-              type='text'
-              label='Last name'
-            />
-          </div>
-          <div className={styles.row}>
-            <Field
-              name='displayName'
-              {...formInputClasses}
-              component={FormInput}
-              type='text'
-              label='Display Name'
-            />
-            <Field
-              name='email'
-              {...formInputClasses}
-              component={FormInput}
-              type='text'
-              label='Email Address'
-            />
-          </div>
-          <div className={styles.row}>
-            <Field
-              name='password'
-              {...formInputClasses}
-              component={FormInput}
-              type='password'
-              label='Password'
-            />
-            <Field
-              name='confirmPassword'
-              {...formInputClasses}
-              component={FormInput}
-              type='password'
-              label='Password confirmation'
-            />
-          </div>
-          <div className={styles.choseRoleContainer}>
-            <Field name='role' type='radio' value={CONSTANTS.CUSTOMER}
-                   strRole='Join As a Buyer'
-                   infoRole='I am looking for a Name, Logo or Tagline for my business, brand or product.'
-                   component={RoleInput} id={CONSTANTS.CUSTOMER}/>
-            <Field name='role' type='radio' value={CONSTANTS.CREATOR}
-                   strRole='Join As a Creative'
-                   infoRole='I plan to submit name ideas, Logo designs or sell names in Domain Marketplace.'
-                   component={RoleInput} id={CONSTANTS.CREATOR}/>
-          </div>
-          <div className={styles.termsOfService}>
-            <Field
-              name='agreeOfTerms'
-              classes={{
-                container: styles.termsOfService,
-                warning: styles.fieldWarning,
-              }}
-              id='termsOfService'
-              component={AgreeTermOfServiceInput}
-              type='checkbox'
-            />
-
-          </div>
-          <button type='submit' disabled={submitting}
-                  className={styles.submitContainer}>
-            <span className={styles.inscription}>Create Account</span>
-          </button>
-        </form>
-      </>
-    );
-  }
+      </div>
+      <button type='submit'
+              disabled={submitting}
+              className={styles.submitContainer}>
+        <span className={styles.inscription}>Create Account</span>
+      </button>
+    </form>
+  );
 }
 
-const mapStateToProps = ( state ) => {
-  return {
-    auth: state.auth,
-    initialValues: {
-      role: CONSTANTS.CUSTOMER,
-    },
-  };
-};
 
-const mapDispatchToProps = ( dispatch ) => (
-  {
-    signUp: ( data ) => dispatch( authActionRegister( data ) ),
-    authClear: () => dispatch( clearAuth() ),
-  }
-);
-
-export default connect( mapStateToProps, mapDispatchToProps )( reduxForm( {
+export default reduxForm( {
   form: 'login',
   validate: customValidator( Schemes.RegistrationSchem ),
-} )( RegistrationForm ) );
+} )( RegistrationForm );
--- client/src/pages/RegistrationPage/RegistrationPage.js
+++ client/src/pages/RegistrationPage/RegistrationPage.js
@@ -1,27 +1,35 @@
-import React                        from 'react';
-import Logo                         from '../../components/Logo';
-import RegistrationForm             from '../../components/RegistrationForm/RegistrationForm';
-import styles                       from './RegistrationPage.module.sass';
-import { Link }                     from 'react-router-dom';
-import { connect }                             from 'react-redux';
-import { clearAuth, clearErrorSignUpAndLogin } from '../../actions/actionCreator';
-import CONSTANTS                               from '../../constants';
-import faq                          from './faq.json'
-import Question                     from "../../components/Question";
-import Error                        from "../../components/Error/Error";
-
+import React       from 'react';
+import Logo        from '../../components/Logo';
+import RegistrationForm
+                   from '../../components/RegistrationForm/RegistrationForm';
+import styles      from './RegistrationPage.module.sass';
+import { Link }    from 'react-router-dom';
+import { connect } from 'react-redux';
+import {
+  authActionRegister,
+  clearAuth,
+  clearErrorSignUpAndLogin
+}                  from '../../actions/actionCreator';
+import CONSTANTS   from '../../constants';
+import faq         from './faq.json'
+import Question    from "../../components/Question";
+import Error       from "../../components/Error/Error";
+import _           from 'lodash';
 
 const RegistrationPage = ( props ) => {
 
-
-  props.clearError();
+  const { authClear, initialValues, error, signUp } = props;
 
   const faqElements = faq.map( ( question, index ) => (
-      <Question key={index}
-                body={question.body}
-                head={question.head}
-                className={styles.questionsContainer}/>)
+    <Question key={index}
+              body={question.body}
+              head={question.head}
+              className={styles.questionsContainer}/> )
   );
+
+  const handleSubmit = ( data ) => {
+    signUp( _.omit( data, [ 'confirmPassword', 'agreeOfTerms' ] ) );
+  };
 
   return (
     <div className={styles.signUpPage}>
@@ -45,7 +53,11 @@ const RegistrationPage = ( props ) => {
         </div>
         <div>
 
-          <RegistrationForm/>
+          {error && <Error data={error.data}
+                           status={error.status}
+                           clearError={authClear}/>}
+
+          <RegistrationForm onSubmit={handleSubmit} initialValues={initialValues}/>
         </div>
       </div>
       <div className={styles.footer}>
@@ -68,11 +80,21 @@ const RegistrationPage = ( props ) => {
 };
 
 
+const mapStateToProps = ( state ) => {
+  return {
+    ...state.auth,
+    initialValues: {
+      role: CONSTANTS.CUSTOMER,
+    },
+  };
+};
+
 const mapDispatchToProps = ( dispatch ) => {
   return {
     authClear: () => dispatch( clearAuth() ),
     clearError: () => dispatch( clearErrorSignUpAndLogin() ),
+    signUp: ( data ) => dispatch( authActionRegister( data ) ),
   };
 };
 
-export default connect( null, mapDispatchToProps )( RegistrationPage );
+export default connect( mapStateToProps, mapDispatchToProps )( RegistrationPage );
