d3070397f938102ea1cd62a785dcffc2e332297c
eveins <maxmost512@gmail.com>
2020-04-17 17:40:57 +0300

    minor refactor: reformat code

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -114,24 +114,24 @@ module.exports.payment = async (req, res, next) => {
   try {
     transaction = await bd.sequelize.transaction();
     await bankQueries.updateBankBalance({
-        balance: bd.sequelize.literal(`
+      balance: bd.sequelize.literal(`
                 CASE
             WHEN "cardNumber"='${ req.body.number.replace(/ /g,
-          '') }' AND "cvc"='${ req.body.cvc }' AND "expiry"='${ req.body.expiry }'
+    '') }' AND "cvc"='${ req.body.cvc }' AND "expiry"='${ req.body.expiry }'
                 THEN "balance"-${ req.body.price }
             WHEN "cardNumber"='${ CONSTANTS.SQUADHELP_BANK_NUMBER }' AND "cvc"='${ CONSTANTS.SQUADHELP_BANK_CVC }' AND "expiry"='${ CONSTANTS.SQUADHELP_BANK_EXPIRY }'
                 THEN "balance"+${ req.body.price } END
         `),
+    },
+    {
+      cardNumber: {
+        [ bd.sequelize.Op.in ]: [
+          CONSTANTS.SQUADHELP_BANK_NUMBER,
+          req.body.number.replace(/ /g, ''),
+        ],
       },
-      {
-        cardNumber: {
-          [ bd.sequelize.Op.in ]: [
-            CONSTANTS.SQUADHELP_BANK_NUMBER,
-            req.body.number.replace(/ /g, ''),
-          ],
-        },
-      },
-      transaction);
+    },
+    transaction);
     const orderId = uuid();
     req.body.contests.forEach((contest, index) => {
       const prize = index === req.body.contests.length - 1 ? Math.ceil(
@@ -185,9 +185,9 @@ module.exports.cashout = async (req, res, next) => {
       { balance: bd.sequelize.literal('balance - ' + req.body.sum) },
       req.tokenData.userId, transaction);
     await bankQueries.updateBankBalance({
-        balance: bd.sequelize.literal(`CASE 
+      balance: bd.sequelize.literal(`CASE 
                 WHEN "cardNumber"='${ req.body.number.replace(/ /g,
-          '') }' AND "expiry"='${ req.body.expiry }' AND "cvc"='${ req.body.cvc }'
+    '') }' AND "expiry"='${ req.body.expiry }' AND "cvc"='${ req.body.cvc }'
                     THEN "balance"+${ req.body.sum }
                 WHEN "cardNumber"='${ CONSTANTS.SQUADHELP_BANK_NUMBER }' AND "expiry"='${ CONSTANTS.SQUADHELP_BANK_EXPIRY }' AND "cvc"='${ CONSTANTS.SQUADHELP_BANK_CVC }'
                     THEN "balance"-${ req.body.sum }
